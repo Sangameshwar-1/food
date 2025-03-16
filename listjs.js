2c9edac7f3df1eebf6247507e7d1b49355e32f27
SALE SANGAMESHWAR <salesangameshwar1@gmail.com>
2025-03-16 20:55:47 +0530

    Update listjs.js

--- listjs.js
+++ listjs.js
@@ -201,7 +201,7 @@
           distances.forEach(item => { // Loop through the distances list
             const listItem = document.createElement('li'); // Create a list item
             listItem.classList.add('donor-item'); 
-            listItem.innerHTML = `<span class="donor-name">${item.donor.name}</span> - ${item.donor.bloodType} - ${item.donor.contact} - ${item.distance} km <div class="address">${item.donor.address}</div>`; // Display the donor's name, blood type, contact, address and distance for each donor according to the distance
+            listItem.innerHTML = `<span class="donor-name">${item.donor.name}</span> - ${item.donor.bloodType} - ${item.donor.contact} - ${item.distance} km <div class="district">${item.donor.district}</div>`; // Display the donor's name, blood type, contact, address and distance for each donor according to the distance
             listItem.querySelector('.donor-name').addEventListener('click', () => { // Add an event listener to the donor's name to highlight the donor and distance and address
               listItem.classList.toggle('highlight'); // Toggle the highlight class
             });
@@ -278,7 +278,7 @@
       currentDonors.forEach(donor => { // Loop through the donors array display the donor's name, blood type, contact and address
         const listItem = document.createElement('li'); // Create a list item
         listItem.classList.add('donor-item'); // Add the donor-item class to the list
-        listItem.innerHTML = `<span class="donor-name">${donor.name}</span> - ${donor.bloodType} - ${donor.contact} <div class="address">${donor.address}</div>`; // Display the donor's name, blood type, contact and address
+        listItem.innerHTML = `<span class="donor-name">${donor.name}</span> - ${donor.bloodType} - ${donor.contact} <div class="district">${donor.district}</div>`; // Display the donor's name, blood type, contact and address
         //>
         listItem.querySelector('.donor-name').addEventListener('click', () => { // Add an event listener to the donor's name to highlight the donor
           listItem.classList.toggle('highlight'); // Toggle the highlight class

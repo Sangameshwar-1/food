1f8cd1d1adfd89d058726a3c5d6bae12389ebadc
SALE SANGAMESHWAR <salesangameshwar1@gmail.com>
2025-03-17 10:28:57 +0530

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -1,13 +1,13 @@
 //firebase configuration
 const firebaseConfig = {
-        apiKey: "AIzaSyAHt06CtReHyQRip-QqEGILFjOWH5cI98c",
-        authDomain: "blood-7b054.firebaseapp.com",
-        databaseURL: "https://blood-7b054-default-rtdb.firebaseio.com",
-        projectId: "blood-7b054",
-        storageBucket: "blood-7b054.firebasestorage.app",
-        messagingSenderId: "926378767902",
-        appId: "1:926378767902:web:21591c4e5d77c90c9ca00f",
-        measurementId: "G-HTGC1SJYH6"
+        apiKey: process.env.REACT_APP_API_KEY,
+            authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+            databaseURL: process.env.REACT_APP_DATABASE_URL,
+            projectId: process.env.REACT_APP_PROJECT_ID,
+            storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+            messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+            appId: process.env.REACT_APP_APP_ID,
+            measurementId: process.env.REACT_APP_MEASUREMENT_ID
       };
 /*
   1. app // Initialize Firebase
